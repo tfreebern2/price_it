a6141e0e1dae696e9510e141d42b0db9d9822e08
Timothy Freebern II <tfreebern2@gmail.com>
2020-05-30 23:57:28 -0400

    Add null checks on current Price

--- lib/datamodels/item.dart
+++ lib/datamodels/item.dart
@@ -1,3 +1,5 @@
+import 'package:priceit/util/constants.dart';
+
 class Item {
   String id;
   String title;
@@ -6,22 +8,30 @@ class Item {
   String viewItemUrl;
   String location;
   String country;
+  String currentPrice;
 
-  Item(this.id,
-      this.title,
-      this.globalId,
-      this.galleryUrl,
-      this.viewItemUrl,
-      this.location,
-      this.country);
+  Item(this.id, this.title, this.globalId, this.galleryUrl, this.viewItemUrl,
+      this.location, this.country, this.currentPrice);
 
   Item.fromMap(Map<String, dynamic> data) {
-    id = data.containsKey("itemId") ? data["itemId"][0] : "";
-    title = data.containsKey("title") ? data["title"][0] : "";
-    globalId = data.containsKey("globalId") ? data["globalId"][0] : "";
-    galleryUrl = data.containsKey("galleryURL") ? data["galleryURL"][0] : "";
-    viewItemUrl = data.containsKey("viewItemURL") ? data["viewItemURL"][0] : "";
-    location = data.containsKey("location") ? data["location"][0] : "";
-    country = data.containsKey("country") ? data["country"][0] : "";
+    id = data.containsKey(itemId) ? data[itemId][0] : notAvailable;
+    title = data.containsKey(titleKey) ? data[titleKey][0] : notAvailable;
+    globalId = data.containsKey(globalIdKey) ? data[globalIdKey][0] : notAvailable;
+    galleryUrl = data.containsKey(galleryUrlKey) ? data[galleryUrlKey][0] : notAvailable;
+    viewItemUrl = data.containsKey(viewItemUrlKey) ? data[viewItemUrlKey][0] : notAvailable;
+    location = data.containsKey(locationKey) ? data[locationKey][0] : notAvailable;
+    country = data.containsKey(countryKey) ? data[countryKey][0] : notAvailable;
+    setCurrentPrice(data);
+  }
+
+  void setCurrentPrice(Map<String, dynamic> data) {
+    data.forEach((key, value) {
+      if (key == sellingStatusKey) {
+        var sellingStatusMap = value[0] as Map<String, dynamic>;
+        currentPrice = sellingStatusMap.containsKey(currentPriceKey)
+            ? data[sellingStatusKey][0][currentPriceKey][0][underscoreValueKey]
+            : notAvailable;
+      }
+    });
   }
 }
